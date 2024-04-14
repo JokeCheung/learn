00345cad8e29987f855829afccbbff5bfe1544d9
joke <1825609597@qq.com>
2024-04-14 22:40:04 +0800

    忽略测试test文件

--- learn/web/big_event/src/main/java/org/example/exception/GlobalExceptionHandler.java
+++ learn/web/big_event/src/main/java/org/example/exception/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+//全局异常处理器
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
