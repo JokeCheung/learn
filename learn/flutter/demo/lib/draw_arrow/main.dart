efbdeb25ec4251034112e8eae143bc28d25f6d93
joke <1825609597@qq.com>
2024-04-24 18:41:23 +0800

    增加箭头绘制

--- learn/flutter/demo/lib/draw_arrow/main.dart
+++ learn/flutter/demo/lib/draw_arrow/main.dart
@@ -1,5 +1,5 @@
+import 'package:demo/draw_arrow/arrow_painter.dart';
 import 'package:flutter/material.dart';
-
 
 void main() {
   runApp(const MyApp());
@@ -27,6 +27,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  Offset? startOffset = Offset.zero;
+  Offset? endOffset = Offset.zero;
 
   @override
   Widget build(BuildContext context) {
@@ -34,8 +36,28 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
       ),
-      body: Container(),
+      body: SizedBox.expand(
+        child: GestureDetector(
+          onScaleStart: (details) {
+            startOffset=details.localFocalPoint;
+
+          },
+          onScaleUpdate: (details) {
+            endOffset=details.localFocalPoint;
+            // print("end=$endOffset");
+            setState(() {});
+          },
+          onScaleEnd: (details) {
+            setState(() {});
+          },
+          child: CustomPaint(
+            painter: ArrowPainter(
+              start: startOffset,
+              end: endOffset,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
-
